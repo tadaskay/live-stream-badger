cd2ad7d6ac2d932be3f81371807e0f038eecf86f
tkrivickas <tkrivickas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-18 11:37:15 +0000

    NF: added options to hide offline streams/images
    git-svn-id: http://plugins.svn.wordpress.org/live-stream-badger/trunk@714858 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- stream-status-widget.php
+++ stream-status-widget.php
@@ -18,7 +18,9 @@ class LSB_Stream_Status_Widget extends WP_Widget {
 	}
 
 	function widget( $args, $instance ) {
-		$display_type = isset( $instance['display_type'] ) ? $instance['display_type'] : 'text';
+		$display_type        = isset ( $instance['display_type'] ) ? $instance['display_type'] : 'text';
+		$hide_offline        = isset ( $instance['hide_offline'] ) ? $instance['hide_offline'] : FALSE;
+		$hide_offline_images = isset ( $instance['hide_offline_images'] ) ? $instance['hide_offline_images'] : FALSE;
 
 		// Get menu items for configured menu
 		$menu_items = !empty( $instance['menu_id'] ) ? wp_get_nav_menu_items( $instance['menu_id'] ) : FALSE;
@@ -34,6 +36,8 @@ class LSB_Stream_Status_Widget extends WP_Widget {
 		if ( !empty( $instance['title'] ) ) {
 			echo $args['before_title'] . $instance['title'] . $args['after_title'];
 		}
+
+		$any_content_rendered = FALSE;
 
 		$core = new LSB_API_Core();
 
@@ -61,11 +65,17 @@ class LSB_Stream_Status_Widget extends WP_Widget {
 				<?php
 				foreach ( $streams as $stream ) {
 					/** @var $stream LSB_Stream */
-					$menu_item = $links[$stream->summary->get_id()];
+					$stream_id = $stream->summary->get_id();
+					$menu_item = isset( $links[$stream_id] ) ? $links[$stream_id] : NULL;
 					if ( empty( $menu_item ) )
 						continue;
 
-					$is_on        = ( $stream->watching_now != -1 );
+					$is_on = ( $stream->watching_now != -1 );
+					if ( !$is_on && $hide_offline )
+						continue;
+
+					$any_content_rendered = TRUE;
+
 					$status_class = $is_on ? 'lsb-on' : 'lsb-off';
 					?>
 					<li class="lsb-status-widget-list-item <?php echo $status_class; ?>">
@@ -73,25 +83,27 @@ class LSB_Stream_Status_Widget extends WP_Widget {
 							<a href="<?php echo $menu_item->url; ?>"
 							   target="_blank"><?php echo apply_filters( 'lsb_stream_status_widget_text', $menu_item->title ); ?></a>
 						</span>
-						<span
-							class="lsb-status-widget-indicator <?php echo $status_class; ?>"><?php echo $is_on ? $stream->watching_now : 'Offline'; ?></span>
+						<span class="lsb-status-widget-indicator <?php echo $status_class; ?>"><?php echo $is_on ? $stream->watching_now : 'Offline'; ?></span>
 						<?php
-						if ( $is_on && $display_type == 'screen_cap' && !empty( $stream->screen_cap_url ) ) {
-							?>
-							<span class="lsb-status-widget-image">
+						if ( $is_on || !$hide_offline_images ) {
+							if ( $display_type == 'screen_cap' && !empty( $stream->screen_cap_url ) ) {
+								?>
+								<span class="lsb-status-widget-image">
 								<a href="<?php echo $menu_item->url; ?>" target="_blank">
 									<img src="<?php echo $stream->screen_cap_url; ?>">
 								</a>
 							</span>
-						<?php
-						} else if ( $display_type == 'image' && !empty ( $stream->image_url)) {
-							?>
-							<span class="lsb-status-widget-image">
+							<?php
+							}
+							else if ( $display_type == 'image' && !empty ( $stream->image_url ) ) {
+								?>
+								<span class="lsb-status-widget-image">
 								<a href="<?php echo $menu_item->url; ?>" target="_blank">
 									<img src="<?php echo $stream->image_url; ?>">
 								</a>
 							</span>
 							<?php
+							}
 						}
 						?>
 					</li>
@@ -99,6 +111,13 @@ class LSB_Stream_Status_Widget extends WP_Widget {
 				}
 				?>
 			</ul>
+			<?php
+			if ( !$any_content_rendered ) {
+				?>
+				<span class="lsb-status-widget-info"><?php _e( 'No streams available' ); ?></span>
+			<?php
+			}
+			?>
 		</div>
 
 		<?php
@@ -108,17 +127,23 @@ class LSB_Stream_Status_Widget extends WP_Widget {
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
 
-		$instance['title']        = strip_tags( stripslashes( $new_instance['title'] ) );
-		$instance['menu_id']      = (int) $new_instance['menu_id'];
+		$instance['title']   = strip_tags( stripslashes( $new_instance['title'] ) );
+		$instance['menu_id'] = (int) $new_instance['menu_id'];
+
 		$instance['display_type'] = $new_instance['display_type'];
+
+		$instance['hide_offline']        = $new_instance['hide_offline'];
+		$instance['hide_offline_images'] = $new_instance['hide_offline_images'];
 
 		return $instance;
 	}
 
 	function form( $instance ) {
-		$title        = isset( $instance['title'] ) ? $instance['title'] : '';
-		$menu_id      = isset( $instance['menu_id'] ) ? $instance['menu_id'] : '';
-		$display_type = isset ( $instance['display_type'] ) ? $instance['display_type'] : 'text';
+		$title               = isset ( $instance['title'] ) ? $instance['title'] : '';
+		$menu_id             = isset ( $instance['menu_id'] ) ? $instance['menu_id'] : '';
+		$display_type        = isset ( $instance['display_type'] ) ? $instance['display_type'] : 'text';
+		$hide_offline        = isset ( $instance['hide_offline'] ) ? $instance['hide_offline'] : FALSE;
+		$hide_offline_images = isset ( $instance['hide_offline_images'] ) ? $instance['hide_offline_images'] : FALSE;
 
 		$menus = get_terms( 'nav_menu', array( 'hide_empty' => FALSE ) );
 
@@ -137,7 +162,7 @@ class LSB_Stream_Status_Widget extends WP_Widget {
 		</p>
 		<p>
 			<label name="<?php echo $this->get_field_id( 'menu_id' ); ?>"><?php _e( 'Select menu:' ); ?></label>
-			<select class="widefat" name="<?php echo $this->get_field_name( 'menu_id' ); ?>"
+			<select name="<?php echo $this->get_field_name( 'menu_id' ); ?>"
 			        id="<?php echo $this->get_field_id( 'menu_id' ); ?>">
 				<?php
 				foreach ( $menus as $menu ) {
@@ -145,14 +170,27 @@ class LSB_Stream_Status_Widget extends WP_Widget {
 				}
 				?>
 			</select>
-
-			<label name="<?php echo $this->get_field_id( 'display_type' ); ?>"><?php _e( 'Display type' ); ?></label>
-			<select class="widefat" name="<?php echo $this->get_field_name( 'display_type' ); ?>""
+		</p>
+		<p>
+			<label name="<?php echo $this->get_field_id( 'display_type' ); ?>"><?php _e( 'Display type:' ); ?></label>
+			<select name="<?php echo $this->get_field_name( 'display_type' ); ?>""
 			id="<?php echo $this->get_field_id( 'display_type' ); ?>">
 			<option value="text" <?php selected( $display_type, 'text' ) ?>>Text</option>
 			<option value="screen_cap" <?php selected( $display_type, 'screen_cap' ) ?>>Screen Capture</option>
 			<option value="image" <?php selected( $display_type, 'image' ) ?>>Channel's image</option>
 			</select>
+		</p>
+		<p>
+			<label name="<?php echo $this->get_field_id( 'hide_offline' ); ?>"><?php _e( 'Hide offline?' ); ?></label>
+			<input type="checkbox" id="<?php echo $this->get_field_id( 'hide_offline' ); ?>"
+			       name="<?php echo $this->get_field_name( 'hide_offline' ); ?>" <?php checked( $hide_offline ) ?>
+			       value="1"/>
+		</p>
+		<p>
+			<label name="<?php echo $this->get_field_id( 'hide_offline_images' ); ?>"><?php _e( 'Hide offline images?' ); ?></label>
+			<input type="checkbox" id="<?php echo $this->get_field_id( 'hide_offline_images' ); ?>"
+			       name="<?php echo $this->get_field_name( 'hide_offline_images' ); ?>" <?php checked( $hide_offline_images ) ?>
+			       value="1"/>
 		</p>
 	<?php
 	} // form()
